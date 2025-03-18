3285c4c1fb28827f274e008ad4d722b9120816ad
Your Name <your.email@example.com>
2025-03-18 22:05:31 +0500

    add

--- src/components/Header&Footer/Footer/Navbar.jsx
+++ src/components/Header&Footer/Footer/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
         <div className="flex mt-6 overflow-hidden flex-col justify-center px-14 py-6 bg-slate-900 max-md:px-5">
             <div className="flex flex-col justify-center w-full max-md:max-w-full">
                 <header className="flex flex-wrap gap-4 md:gap-10 justify-between items-center w-full max-md:max-w-full">
-                    <div className="flex flex-wrap gap-4 w-full md:gap-10 justify-between items-center self-stretch my-auto text-white min-w-60 w-[775px] max-md:max-w-full">
+                    <div className="flex flex-wrap gap-4 md:gap-10 justify-between items-center self-stretch my-auto text-white min-w-60 w-[775px] max-md:max-w-full">
                         <Logo />
                         <NavLinks />
                     </div>

a3986cc093a461878972dbedea6e6d450805db46
Your Name <your.email@example.com>
2025-03-19 15:36:27 +0500

    add

--- src/components/LandingPage/HeroSection.jsx
+++ src/components/LandingPage/HeroSection.jsx
@@ -9,7 +9,7 @@ function HeroSection() {
         <>
             {/* <BackgroundSVG /> */}
 
-            <section className="flex z-10 pt-10 flex-col gap-10 items-center w-full min-h-[screen]">
+            <section className="flex z-10 pt-2 flex-col gap-10 items-center w-full min-h-[screen]">
                 <header className="flex flex-col gap-5 items-center px-5 pt-10 w-full">
                     <div className="flex flex-col gap-3 justify-center items-center w-full max-w-[1240px]">
                         <h1 className="text-4xl font-bold text-center text-white leading-[60px] max-md:text-5xl max-md:leading-[40px] max-sm:text-4xl max-sm:leading-10">

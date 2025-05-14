8dffcb6e64bbbd6eca7720e4a7b18c9e5637cfb8
Your Name <your.email@example.com>
2025-05-14 08:18:53 +0500

    logo and email, supportemails

--- src/components/Header&Footer/Footer/NavLinks.jsx
+++ src/components/Header&Footer/Footer/NavLinks.jsx
@@ -1,46 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavLinks() {
-    return (
-        <nav className="flex flex-wrap gap-3 justify-center items-center self-stretch my-auto text-sm font-semibold min-w-60 max-md:max-w-full">
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
-            >
-                Home
-            </a>
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch px-6 py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[82px] max-md:px-5"
-            >
-                news
-            </a>
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch px-6 py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[89px] max-md:px-5"
-            >
-                games
-            </a>
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch py-4 pr-4 pl-4 my-auto bg-slate-900 min-h-10 w-[115px]"
-            >
-                betting odds
-            </a>
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch py-4 pr-4 pl-5 my-auto whitespace-nowrap bg-slate-900 min-h-10 w-[97px]"
-            >
-                education
-            </a>
-            <a
-                href="#"
-                className="overflow-hidden gap-2.5 self-stretch px-3.5 py-4 my-auto bg-slate-900 min-h-10 w-[97px]"
-            >
-                contact us
-            </a>
-        </nav>
-    );
+  return (
+    <nav className="flex flex-wrap gap-3 justify-center items-center self-stretch my-auto text-sm font-semibold min-w-60 max-md:max-w-full">
+      <NavLink
+        className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
+        to="/news"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
+        to="/news"
+      >
+        News
+      </NavLink>
+      <NavLink
+        className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
+        to="/games"
+      >
+        Games
+      </NavLink>
+      <NavLink
+        className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
+        to="/contact"
+      >
+        Contact
+      </NavLink>
+      <NavLink
+        className="overflow-hidden gap-2.5 self-stretch py-4 my-auto text-center whitespace-nowrap bg-slate-900 min-h-10 w-[62px]"
+        to="/pricing"
+      >
+        Pricing
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavLinks;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,39 +1,39 @@
-import { useNavigate } from 'react-router-dom';
-import SignUpComponent from '../components/Auth/SignUpComponent/SignUpComponent';
+import { useNavigate } from "react-router-dom";
+import SignUpComponent from "../components/Auth/SignUpComponent/SignUpComponent";
 
 const SignUpPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className=" flex flex-col-reverse md:grid md:grid-cols-12 overflow-hidden">
-            <div
-                className="absolute top-5 left-5 z-50  cursor-pointer"
-                onClick={() => navigate('/')}
-            >
-                <img
-                    src="/assets/shield.png" // Replace with the correct path
-                    alt="Home"
-                    className="w-12 h-12"
-                />
-            </div>
-            {/* Left Section */}
-            <div className="relative col-span-7 hidden md:block  h-screen">
-                <img
-                    src="/assets/sign-up.png" // Updated path
-                    alt="Signup"
-                    className="w-full h-full object-cover"
-                    loading="lazy" // Added lazy loading
-                />
-                {/* Dark overlay */}
-                <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-            </div>
+  return (
+    <div className=" flex flex-col-reverse md:grid md:grid-cols-12 overflow-hidden">
+      <div
+        className="absolute top-5 left-5 z-50  cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img
+          src="/assets/logo.png" // Replace with the correct path
+          alt="Home"
+          className="w-12"
+        />
+      </div>
+      {/* Left Section */}
+      <div className="relative col-span-7 hidden md:block  h-screen">
+        <img
+          src="/assets/sign-up.png" // Updated path
+          alt="Signup"
+          className="w-full h-full object-cover"
+          loading="lazy" // Added lazy loading
+        />
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+      </div>
 
-            {/* Right Section */}
-            {/* <div className="flex items-center justify-center bg-gradient-to-b from-blue-800 to-blue-900 h-[60vh] lg:h-screen"> */}
-            <SignUpComponent />
-            {/* </div> */}
-        </div>
-    );
+      {/* Right Section */}
+      {/* <div className="flex items-center justify-center bg-gradient-to-b from-blue-800 to-blue-900 h-[60vh] lg:h-screen"> */}
+      <SignUpComponent />
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default SignUpPage;

372bf052d9f9adf5eb422872696800911d374f38
Your Name <your.email@example.com>
2025-06-13 18:14:05 +0500

    Footer links fix

--- src/components/Header&Footer/Footer/Footer.jsx
+++ src/components/Header&Footer/Footer/Footer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 function Footer() {
   return (
     <footer className="flex flex-col justify-center mt-6 w-full text-sm leading-loose text-center text-slate-600 max-md:max-w-full">
       <div className="flex w-full bg-white min-h-px max-md:max-w-full" />
       <p className="mt-6 max-md:max-w-full">
-        © Typerline. 2025-2026. All rights reserved.
+        © Typerline. 2025. All rights reserved.
       </p>
     </footer>
   );
--- src/components/Header&Footer/Footer/Logo.jsx
+++ src/components/Header&Footer/Footer/Logo.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Logo() {
+  const handleScrollTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
   return (
-    <NavLink to="/">
+    <NavLink to="/" onClick={handleScrollTop}>
       <img src="/assets/logo.png" alt="Logo" className="w-12 hidden lg:block" />
     </NavLink>
   );
